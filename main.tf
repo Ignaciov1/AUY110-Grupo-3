207b83316f8e0931a173db40ee90efea3b48e02e
Ignaciov1 <ig.sariego@duocuc.cl>
2026-05-28 22:34:00 -0400

    fix: aplica auto-formato en main.tf para aprobar validación de pipeline e iniciar despliegue en AWS

--- main.tf
+++ main.tf
@@ -9,16 +9,16 @@ data "aws_iam_role" "lab_role" {
 
 # 1. Llamada al Módulo de Redes (VPC)
 module "redes" {
-  source       = "git::https://github.com/Ignaciov1/terraform-aws-vpc-AUY1105-grupo-3.git?ref=v0.1.0"
-  
+  source = "git::https://github.com/Ignaciov1/terraform-aws-vpc-AUY1105-grupo-3.git?ref=v0.1.0"
+
   # Pasamos la variable obligatoria
   lab_role_arn = data.aws_iam_role.lab_role.arn
 }
 
 # 2. Llamada al Módulo de Cómputo (EC2)
 module "computo" {
-  source            = "git::https://github.com/Ignaciov1/terraform-aws-ec2-AUY1105-grupo-3.git?ref=v0.1.0"
-  
+  source = "git::https://github.com/Ignaciov1/terraform-aws-ec2-AUY1105-grupo-3.git?ref=v0.1.0"
+
   # Conectamos EC2 con las salidas (outputs) del módulo de red
   subnet_id         = module.redes.public_subnet_1_id
   security_group_id = module.redes.security_group_ssh_id
